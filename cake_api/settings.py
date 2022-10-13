7d05518b9fed9ee1b7e53dbec3952add2ad62969
TomTom2k <you@example.com>
2022-10-13 12:20:14 +0700

    deploy 6

--- cake_api/settings.py
+++ cake_api/settings.py
@@ -131,11 +131,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
+STATIC_URL = '/static/'
+
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
@@ -147,5 +146,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "user.User"
+DISABLE_COLLECTSTATIC = 1
 
 CORS_ALLOW_ALL_ORIGINS = True
