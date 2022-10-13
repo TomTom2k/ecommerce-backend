9bef6661bd7299a10b6159c193f3cc2337d694d0
TomTom2k <you@example.com>
2022-10-13 10:55:03 +0700

    deploy 1

--- cake_api/settings.py
+++ cake_api/settings.py
@@ -136,9 +136,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
-# django_heroKu.settings(locals())
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
+django_heroKu.settings(locals())
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
